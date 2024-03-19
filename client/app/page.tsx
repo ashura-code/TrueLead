ce9513b576bb6cf869da5397157cd0edbebeb5cf
adhitya nantish <adhitya2k3@gmail.com>
2024-03-19 11:01:37 +0530

    responsive gallery ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
-import UploadImage from '@/comps/UploadImage'
+"use client";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import UploadImage from "@/comps/UploadImage";
+import UploadPhoto from "../components/component/upload-photo";
 export default function Home() {
-  return <div>
+  return (
+    <div className="w-[100vh]">
+      <section className="mx-10 my-6">
+        <UploadPhoto />
+      </section>
 
-     <UploadImage/>
-
-  </div>;
+      <UploadImage />
+    </div>
+  );
 }
